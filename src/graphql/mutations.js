5d9363897caabb0064a5d620c7ad59406a8d5646
fujiokayu <fujiokayu@gmail.com>
2020-02-23 16:06:15 +0900

    done step 3

---
+++ src/graphql/mutations.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createMyType = /* GraphQL */ `
+  mutation CreateMyType(
+    $input: CreateMyTypeInput!
+    $condition: ModelMyTypeConditionInput
+  ) {
+    createMyType(input: $input, condition: $condition) {
+      id
+      title
+      content
+      price
+      rating
+    }
+  }
+`;
+export const updateMyType = /* GraphQL */ `
+  mutation UpdateMyType(
+    $input: UpdateMyTypeInput!
+    $condition: ModelMyTypeConditionInput
+  ) {
+    updateMyType(input: $input, condition: $condition) {
+      id
+      title
+      content
+      price
+      rating
+    }
+  }
+`;
+export const deleteMyType = /* GraphQL */ `
+  mutation DeleteMyType(
+    $input: DeleteMyTypeInput!
+    $condition: ModelMyTypeConditionInput
+  ) {
+    deleteMyType(input: $input, condition: $condition) {
+      id
+      title
+      content
+      price
+      rating
+    }
+  }
+`;
