b856ac518eb137f917102459e96dcc409a5c48e2
fujiokayu <fujiokayu@gmail.com>
2020-03-01 12:57:14 +0900

    Fix tab space to 4

--- src/App.js
+++ src/App.js
@@ -16,47 +16,48 @@ Amplify.configure(awsconfig);
 const QUERY = 'QUERY';
 
 const initialState = {
-  todos: [],
+    todos: [],
 };
 
 const reducer = (state, action) => {
-  switch (action.type) {
-    case QUERY:
-      return {...state, todos: action.todos};
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case QUERY:
+            return {...state, todos: action.todos};
+        default:
+            return state;
+    }
 };
 
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-  useEffect(() => {
-    async function getData() {
-      const todoData = await API.graphql(graphqlOperation(listTodos));
-      dispatch({ type: QUERY, todos: todoData.data.listTodos.items });
-    }
-    getData();
-  }, []);
-
-  return (
-    <div className="App">
-      <h1 className="siimple--color-primary">Amplify Goat</h1>
-      <h4 className="siimple--color-dark">Vulnerable web app by using Amplify.</h4>
-      <TodoForm/>
-      <div className="siimple--color-dark">
-        {state.todos.length > 0 ? 
-          state.todos.map((todo) => <p key={todo.id}>{todo.name} : {todo.description}</p>) :
-          <p>Add some todos!</p> 
+    useEffect(() => {
+        async function getData() {
+            const todoData = await API.graphql(graphqlOperation(listTodos));
+            dispatch({ type: QUERY, todos: todoData.data.listTodos.items });
         }
-      </div>
-    </div>
-  );
+        getData();
+    }, []);
+
+    return (
+        <div className="App">
+            <h1 className="siimple--color-primary">Amplify Goat</h1>
+            <h4 className="siimple--color-dark">Vulnerable web app by using Amplify.</h4>
+            <TodoForm/>
+            <div className="siimple--color-dark">
+                {state.todos.length > 0 ? 
+                    state.todos.map((todo) => <p key={todo.id}>{todo.name} : {todo.description}</p>) :
+                    <p>Add some todos!</p> 
+                }
+            </div>
+        </div>
+    );
 }
 
 export default withAuthenticator(App, {
-  // Render a sign out button once logged in
-  includeGreetings: true,
-  signUpConfig
+    // Render a sign out button once logged in
+    includeGreetings: true,
+    signUpConfig
 });
+  
--- src/form.js
+++ src/form.js
@@ -6,45 +6,45 @@ import { createTodo } from './graphql/mutations';
 async function _getUserName() {
     const tokens = await Amplify.Auth.currentSession();
     return tokens.getIdToken().payload['cognito:username'];
-  }
-  
+}
+    
 export class TodoForm extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        value: ''
-      };
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+        super(props);
+        this.state = {
+            value: ''
+        };
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
   
     handleChange(event) {
-      this.setState({value: event.target.value});
+        this.setState({value: event.target.value});
     }
-  
+    
     async handleSubmit(event) {
-      if (this.state.value === '' || this.state.value === undefined) {
-        return
-      }
-      const userName = await _getUserName();
-      const todo = { name: userName, description: this.state.value };
-      await API.graphql(graphqlOperation(createTodo, { input: todo }));
+        if (this.state.value === '' || this.state.value === undefined) {
+            return
+        }
+        const userName = await _getUserName();
+        const todo = { name: userName, description: this.state.value };
+        await API.graphql(graphqlOperation(createTodo, { input: todo }));
       
-      console.log('submitted: ' + this.state.value);
-      event.preventDefault();
+        console.log('submitted: ' + this.state.value);
+        event.preventDefault();
     }
   
     render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label className="siimple-label siimple--color-brack">
-            <input type="text" value={this.state.value} onChange={this.handleChange} className="siimple-input siimple-input--fluid" placeholder="Your Todo"/>
-          </label>
-          <input type="submit" value="Add Todo" className="siimple-btn siimple-btn--primary"/>
-        </form>
-      );
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label className="siimple-label siimple--color-brack">
+                    <input type="text" value={this.state.value} onChange={this.handleChange} className="siimple-input siimple-input--fluid" placeholder="Your Todo"/>
+                </label>
+                <input type="submit" value="Add Todo" className="siimple-btn siimple-btn--primary"/>
+            </form>
+        );
     }
-  }
+}
 
-  export default TodoForm
+export default TodoForm
