79120546222d5290b75c9cb163a4599b7ea7f312
fujiokayu <fujiokayu@gmail.com>
2020-04-12 18:30:33 +0900

    Fix separation of concerns

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import React, { useEffect, useReducer } from 'react'
+import React from 'react'
 import Amplify from 'aws-amplify'
 import { withAuthenticator } from 'aws-amplify-react'
-import API, { graphqlOperation } from '@aws-amplify/api'
-
-import { listTodos } from './graphql/queries'
 
 import awsconfig from './aws-exports'
 import './App.css'
@@ -13,39 +10,13 @@ import List from './list'
 
 Amplify.configure(awsconfig)
 
-// Action Types
-const QUERY = 'QUERY'
-
-const initialState = {
-  todos: [],
-}
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case QUERY:
-      return { ...state, todos: action.todos }
-    default:
-      return state
-  }
-}
-
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState)
-
-  useEffect(() => {
-    async function getData() {
-      const todoData = await API.graphql(graphqlOperation(listTodos))
-      dispatch({ type: QUERY, todos: todoData.data.listTodos.items })
-    }
-    getData()
-  }, [])
-
   return (
     <div className="App">
       <h1 className="siimple--color-primary">Amplify Goat</h1>
       <h4 className="siimple--color-dark">Vulnerable web app by using Amplify.</h4>
       <TodoForm />
-      <List todos={state.todos} />
+      <List todos />
     </div>
   )
 }
--- src/list.js
+++ src/list.js
@@ -1,17 +1,48 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
+import API, { graphqlOperation } from '@aws-amplify/api'
 
-const List = props => (
-  <div className="siimple--color-dark">
-    {props.todos.length > 0 ? (
-      props.todos.map(todo => (
-        <p key={todo.id}>
-          {todo.name} : {todo.description}
-        </p>
-      ))
-    ) : (
-      <p>Add some todos!</p>
-    )}
-  </div>
-)
+import { listTodos } from './graphql/queries'
+
+// Action Types
+const QUERY = 'QUERY'
+
+const initialState = {
+  todos: [],
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case QUERY:
+      return { ...state, todos: action.todos }
+    default:
+      return state
+  }
+}
+
+const List = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    async function getData() {
+      const todoData = await API.graphql(graphqlOperation(listTodos))
+      dispatch({ type: QUERY, todos: todoData.data.listTodos.items })
+    }
+    getData()
+  }, [])
+
+  return (
+    <div className="siimple--color-dark">
+      {state.todos.length > 0 ? (
+        state.todos.map((todo) => (
+          <p key={todo.id}>
+            {todo.name} : {todo.description}
+          </p>
+        ))
+      ) : (
+        <p>Add some todos!</p>
+      )}
+    </div>
+  )
+}
 
 export default List
