82f82e8338238ca9a0fb63c874130e66f908cdbd
fujiokayu <fujiokayu@gmail.com>
2020-04-12 18:13:06 +0900

    Replace class component to function component

--- src/form.js
+++ src/form.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import API, { graphqlOperation } from '@aws-amplify/api'
 import Amplify from 'aws-amplify'
 import { createTodo } from './graphql/mutations'
@@ -8,49 +8,25 @@ async function _getUserName() {
   return tokens.getIdToken().payload['cognito:username']
 }
 
-export class TodoForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: '',
-    }
+const TodoForm = () => {
+  const [description, setDescription] = useState('')
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value })
-  }
-
-  async handleSubmit(event) {
-    if (this.state.value === '' || this.state.value === undefined) {
-      return
-    }
-    const userName = await _getUserName()
-    const todo = { name: userName, description: this.state.value }
-    await API.graphql(graphqlOperation(createTodo, { input: todo }))
-
-    console.log('submitted: ' + this.state.value)
+  const handleSubmit = async (event) => {
     event.preventDefault()
+    if (!description.trim()) return
+    const userName = await _getUserName()
+    const todo = { name: userName, description: description }
+    await API.graphql(graphqlOperation(createTodo, { input: todo }))
   }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label className="siimple-label siimple--color-brack">
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-            className="siimple-input siimple-input--fluid"
-            placeholder="Your Todo"
-          />
-        </label>
-        <input type="submit" value="Add Todo" className="siimple-btn siimple-btn--primary" />
-      </form>
-    )
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label className="siimple-label siimple--color-brack">
+        <input value={description} onChange={(e) => setDescription(e.target.value)} />
+      </label>
+      <input type="submit" value="Add Todo" className="siimple-btn siimple-btn--primary" />
+    </form>
+  )
 }
 
 export default TodoForm
